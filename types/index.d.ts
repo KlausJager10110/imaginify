7add5ccb8a086e27c5a644fc2f3941a516005eaa
Chorn <pacharapolpacharapol2547@gmail.com>
2024-05-10 13:21:13 +0700

    index.d.ts edit

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,157 +1,138 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
-import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
-import { twMerge } from "tailwind-merge";
+/* eslint-disable no-unused-vars */
 
-import { aspectRatioOptions } from "@/constants";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-// ERROR HANDLER
-export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
-    console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
-  } else if (typeof error === "string") {
-    // This is a string error message
-    console.error(error);
-    throw new Error(`Error: ${error}`);
-  } else {
-    // This is an unknown type of error
-    console.error(error);
-    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
-  }
-};
-
-// PLACEHOLDER LOADER - while image is transforming
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#7986AC" offset="20%" />
-      <stop stop-color="#68769e" offset="50%" />
-      <stop stop-color="#7986AC" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#7986AC" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
-
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
-
-export const dataUrl = `data:image/svg+xml;base64,${toBase64(
-  shimmer(1000, 1000)
-)}`;
-// ==== End
-
-// FORM URL QUERY
-export const formUrlQuery = ({
-  searchParams,
-  key,
-  value,
-}: FormUrlQueryParams) => {
-  const params = { ...qs.parse(searchParams.toString()), [key]: value };
-
-  return `${window.location.pathname}?${qs.stringify(params, {
-    skipNulls: true,
-  })}`;
-};
-
-// REMOVE KEY FROM QUERY
-export function removeKeysFromQuery({
-  searchParams,
-  keysToRemove,
-}: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
-
-  keysToRemove.forEach((key) => {
-    delete currentUrl[key];
-  });
-
-  // Remove null or undefined values
-  Object.keys(currentUrl).forEach(
-    (key) => currentUrl[key] == null && delete currentUrl[key]
-  );
-
-  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
-}
-
-// DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+// ====== USER PARAMS
+declare type CreateUserParams = {
+    clerkId: string;
+    email: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    photo: string;
   };
-};
-
-// GE IMAGE SIZE
-export type AspectRatioKey = keyof typeof aspectRatioOptions;
-export const getImageSize = (
-  type: string,
-  image: any,
-  dimension: "width" | "height"
-): number => {
-  if (type === "fill") {
-    return (
-      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-      1000
-    );
-  }
-  return image?.[dimension] || 1000;
-};
-
-// DOWNLOAD IMAGE
-export const download = (url: string, filename: string) => {
-  if (!url) {
-    throw new Error("Resource URL not provided! You need to provide one");
-  }
-
-  fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = blobURL;
-
-      if (filename && filename.length)
-        a.download = `${filename.replace(" ", "_")}.png`;
-      document.body.appendChild(a);
-      a.click();
-    })
-    .catch((error) => console.log({ error }));
-};
-
-// DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
-  if(obj2 === null || obj2 === undefined) {
-    return obj1;
-  }
-
-  let output = { ...obj2 };
-
-  for (let key in obj1) {
-    if (obj1.hasOwnProperty(key)) {
-      if (
-        obj1[key] &&
-        typeof obj1[key] === "object" &&
-        obj2[key] &&
-        typeof obj2[key] === "object"
-      ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
-      } else {
-        output[key] = obj1[key];
-      }
-    }
-  }
-
-  return output;
-};
+  
+  declare type UpdateUserParams = {
+    firstName: string;
+    lastName: string;
+    username: string;
+    photo: string;
+  };
+  
+  // ====== IMAGE PARAMS
+  declare type AddImageParams = {
+    image: {
+      title: string;
+      publicId: string;
+      transformationType: string;
+      width: number;
+      height: number;
+      config: any;
+      secureURL: string;
+      transformationURL: string;
+      aspectRatio: string | undefined;
+      prompt: string | undefined;
+      color: string | undefined;
+    };
+    userId: string;
+    path: string;
+  };
+  
+  declare type UpdateImageParams = {
+    image: {
+      _id: string;
+      title: string;
+      publicId: string;
+      transformationType: string;
+      width: number;
+      height: number;
+      config: any;
+      secureURL: string;
+      transformationURL: string;
+      aspectRatio: string | undefined;
+      prompt: string | undefined;
+      color: string | undefined;
+    };
+    userId: string;
+    path: string;
+  };
+  
+  declare type Transformations = {
+    restore?: boolean;
+    fillBackground?: boolean;
+    remove?: {
+      prompt: string;
+      removeShadow?: boolean;
+      multiple?: boolean;
+    };
+    recolor?: {
+      prompt?: string;
+      to: string;
+      multiple?: boolean;
+    };
+    removeBackground?: boolean;
+  };
+  
+  // ====== TRANSACTION PARAMS
+  declare type CheckoutTransactionParams = {
+    plan: string;
+    credits: number;
+    amount: number;
+    buyerId: string;
+  };
+  
+  declare type CreateTransactionParams = {
+    stripeId: string;
+    amount: number;
+    credits: number;
+    plan: string;
+    buyerId: string;
+    createdAt: Date;
+  };
+  
+  declare type TransformationTypeKey =
+    | "restore"
+    | "fill"
+    | "remove"
+    | "recolor"
+    | "removeBackground";
+  
+  // ====== URL QUERY PARAMS
+  declare type FormUrlQueryParams = {
+    searchParams: string;
+    key: string;
+    value: string | number | null;
+  };
+  
+  declare type UrlQueryParams = {
+    params: string;
+    key: string;
+    value: string | null;
+  };
+  
+  declare type RemoveUrlQueryParams = {
+    searchParams: string;
+    keysToRemove: string[];
+  };
+  
+  declare type SearchParamProps = {
+    params: { id: string; type: TransformationTypeKey };
+    searchParams: { [key: string]: string | string[] | undefined };
+  };
+  
+  declare type TransformationFormProps = {
+    action: "Add" | "Update";
+    userId: string;
+    type: TransformationTypeKey;
+    creditBalance: number;
+    data?: IImage | null;
+    config?: Transformations | null;
+  };
+  
+  declare type TransformedImageProps = {
+    image: any;
+    type: string;
+    title: string;
+    transformationConfig: Transformations | null;
+    isTransforming: boolean;
+    hasDownload?: boolean;
+    setIsTransforming?: React.Dispatch<React.SetStateAction<boolean>>;
+  };
