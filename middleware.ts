31d1b0909e36e308638be1d3ccef07cded29d680
Chorn <pacharapolpacharapol2547@gmail.com>
2024-05-10 14:19:22 +0700

    clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ['/api/webhooks/clerk'],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
