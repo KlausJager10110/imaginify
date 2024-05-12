e985578ddbe3e07c4f6e6cdb2c0dbbd8d3d1e606
Chorn <pacharapolpacharapol2547@gmail.com>
2024-05-12 15:43:29 +0700

    search bax and home page

--- lib/actions/image.action.ts
+++ lib/actions/image.action.ts
@@ -7,6 +7,7 @@ import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 
+import { v2 as cloudinary } from "cloudinary";
 
 const populateUser = (query: any) => query.populate({
     path: 'author',
@@ -96,4 +97,63 @@ export async function getImageById(imageId: string) {
     }
 }
 
+// GET IMAGEs
+export async function getAllImages({ limit = 9, page = 1, searchQuery = '' } : {
+    limit?: number;
+    page: number;
+    searchQuery?: string;
+}) {
+    try {
+        await connectToDatabase();
 
+        cloudinary.config({
+            cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+            secure: true,
+        })
+
+        let expresstion = 'folder=imaginify' //ใช้ชื่อ folder ตามที่ตั้งไว้ใน upload preset บน cloudinary ในที่นี้คือ imaginify
+
+        if (searchQuery) { //หากมีการค้นหา
+            expresstion += ` AND ${searchQuery}`
+        }
+
+        const { resources } = await cloudinary.search
+            .expression(expresstion) //แสดงออกผลลัพท์
+            .execute();
+
+        const resourcesIds = resources.map((resource: any) => resource.public_id);
+
+        let query = {};
+
+        if (searchQuery) { //หากมีการค้นหา
+            
+            query = { 
+                publicId: {
+                    $in: resourcesIds
+                }
+            }
+            console.log(resourcesIds)
+        }
+
+        const skipAmount = (Number(page) -1) * limit; //น่าจะเกี่ยวกับการกดเลือกหน้า
+
+        const images = await populateUser(Image.find(query))
+            .sort({ updatedAt: -1 })
+            .skip(skipAmount)
+            .limit(limit);
+
+        const totalImages = await Image.find(query).countDocuments();
+        const saveImages = await Image.find().countDocuments()
+
+        return {
+            data: JSON.parse(JSON.stringify(images)),
+            totalPage: Math.ceil(totalImages / limit),
+            saveImages,
+        }
+
+    } catch (error) {
+        handleError(error)
+    }
+}
